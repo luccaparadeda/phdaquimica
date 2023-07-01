c2e3994ea03e5f9ccfc007179ffe735591f76004
Lucca Paradeda <luccatisserparadeda@gmail.com>
2023-07-01 17:23:13 -0300

    feat: loading

--- src/app/extensivo/loading.tsx
+++ src/app/extensivo/loading.tsx
@@ -5,8 +5,8 @@ interface loadingProps {}
 
 const loading: FC<loadingProps> = ({}) => {
 	return (
-		<div className='w-full h-screen items-center justify-center'>
-			<Loader2 className='animate-spin h-1/2 w-1/2 text-indigo-800/75' />
+		<div className='flex w-full h-screen items-center justify-center'>
+			<Loader2 className='animate-spin h-1/2 w-1/2 text-gray-800/75' />
 		</div>
 	);
 };
